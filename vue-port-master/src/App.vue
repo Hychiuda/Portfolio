e7c7a3aee4175c918ff284a046c9e6b016883348
Hychiuda <56297557+Hychiuda@users.noreply.github.com>
2020-12-08 13:17:07 +0100

    Add files via upload

---
+++ vue-port-master/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">
+      <img class="logo" alt="logo" src="./assets/logo.png">
+      </router-link>
+      <ul>
+        <li><router-link to="/" class="active">About</router-link></li>
+        <li><router-link to="work">Work</router-link></li>
+      </ul> 
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+#nav{
+  background-color: #d4dce8;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 125px;
+  padding: 0 100px;
+  box-sizing: border-box;
+  transition: .5s;
+}
+
+#nav .logo {
+  float: left;
+  line-height: 0px;
+  margin-top: 12px; 
+  margin-left: 0px;
+  transition: .5s;
+  height: 75%;
+  width: auto;      
+}
+
+#nav .logo img{
+  transition: .5s;    
+}
+
+#nav ul{
+  float: right;
+  padding: 0;
+  display: flex;    
+}
+
+#nav ul li{
+  list-style: none;
+}
+
+#nav ul li a{
+  line-height: 80px;
+  color: #4d648e;
+  padding: 5px 20px;
+  text-decoration: none;
+  transition: .5s;
+  font-family: "Nexa", Courier, monospace;
+  font-size: 20px;
+}
+
+#nav ul li a:hover{
+  color: #1e1f26;
+}
+</style>
---
+++ vue-port-master/src/views/Home.vue
@@ -0,0 +1,109 @@
+<template>
+<body>
+  <div class="primbox">
+    <div id="overline">
+      <img src="@/assets/headline.png" width="40%" height="">
+    </div>
+
+    <div id="underline">a multimediadesign student.</div>
+
+    <img src='@/assets/landscape2.png' alt="background wallpaper" width="100%" height="auto">
+
+    <div id="hi">
+      <img alt="overline" src="@/assets/hi.png" width="10%" height="10%">
+    </div>
+
+    <div class="secbox">                                             
+      <div id="aboutme">I'm a multimediadesigner based in Esbjerg, Denmark. I love being creative, hence my choice of education.
+        I am currently learning editing, graphical design and coding. I love art so next I wanna learn music production, animation and 3d modelling.<br><br>
+        For business inquiries, you can contact my mail: jaco7774@hotmail.com.<br><br><br><br>
+        Below you'll see my business card, my cv and video cv.
+      </div>
+                 
+    </div>
+
+    <div class="cv">
+      <img alt="overline" src="@/assets/business.png" width="25%" height="25%">
+      <img alt="overline" src="@/assets/business2.png" width="25%" height="25%">
+    </div>
+
+    <div class="cv">
+      <img alt="overline" src="@/assets/cv.png" width="40%" height="50%">
+    </div>
+
+    <div class="video">
+    <iframe width="1280" height="720" src="https://www.youtube.com/embed/gbzXv6G9LCA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+  </div>                                     
+</body>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss">
+body{
+  margin: 0;
+}
+
+.primbox{
+  background-color: #d4dce8;
+  width: 100%;
+  height: 500px;   
+}
+
+.secbox{
+  background-color: #fff;
+  width: 100%;
+  height: 400px;
+  display: flex;
+  justify-content: space-between;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+
+#overline{
+  color: #313131;
+  text-align: center;
+  font-size: 0px;
+}
+
+#underline{ 
+  color: #4d648d;
+  font-family: "Nexa", Courier, monospace;
+  text-align: center;
+  font-size: 25px;
+}
+
+#hi{ 
+  text-align: center;
+  margin-top: 125px;   
+}
+
+#aboutme{
+  color: #545454;
+  font-family: "Nexa", Courier, monospace;
+  font-size: 20px;
+  width: 30%;
+  height: 100px;
+  margin-top: 50px;
+  line-height: 150%;
+  text-align: center;
+}
+
+.video{
+  margin-top: 50px;
+  margin-bottom: 50px;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+
+.cv{
+  margin-top: 50px;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+</style>
